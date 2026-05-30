1c6be08781ac0c5d945d04c3ccad06ebcbc0040b
Oscar Matallana <omatalla@c4a7c10.42berlin.de>
2026-05-30 13:12:52 +0200

    fixed ft_bzero.c and updated libft.h

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,29 +6,29 @@
 /*   By: omatalla <omatalla@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 17:08:29 by omatalla          #+#    #+#             */
-/*   Updated: 2026/05/04 17:31:28 by omatalla         ###   ########.fr       */
+/*   Updated: 2026/05/30 13:10:24 by omatalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stddef.h>
-//#include <stdio.h> for printf
-//#include <string.h> for bzero
-//#include <unistd.h> for write
+//#include <stdio.h> // for printf
+//#include <string.h> // for bzero
+//#include <unistd.h> // for write
 
 #include "libft.h"
 
-void	*ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	unsigned char	*ptr;
+	size_t			i;
 
-	ptr = s;
-	while (n > 0)
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
 	{
-		*ptr = '\0';
-		ptr++;
-		n--;
+		ptr[i] = '\0';
+		i++;
 	}
-	return (s);
 }
 /*
 int	main(void)
